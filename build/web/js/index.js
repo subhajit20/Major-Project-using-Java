1e5863200e81c7eaf2fc24a02e9bcef42ea1193f
Subhajit Ghosh <subhajitstd07@gmail.com>
2023-05-09 02:00:57 +0530

    Initialize major project

---
+++ build/web/js/index.js
@@ -0,0 +1,17 @@
+//const btn = document.getElementById("btn");
+//
+//btn.addEventListener('click',async (e)=>{
+////    e.preventDefault();
+//    console.log("Okk")
+//    const res = await fetch("http://localhost:8080/Major_Project/login",{
+//        method:"POST",
+//        headers:{
+//            'content-type':'application/json'
+//        }
+//    });
+//    const data = await res.json();
+//    
+//    console.log(data);
+//})
+
+console.log("Major Project...")
